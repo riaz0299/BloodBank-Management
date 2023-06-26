2ee2977b5ba2a1b8f7c97c4d370c4849c613896d
Hasan Mahadi Riaz <55849338+riaz0299@users.noreply.github.com>
2023-06-26 22:55:02 +0600

    Add files via upload

---
+++ bbdms/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; RIAZ: BloodBank & Donor Management System 2022</p>
+        </div>
+        <!-- /.container -->
+    </footer>
